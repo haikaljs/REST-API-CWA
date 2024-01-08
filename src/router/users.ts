a306911f212cda1a094edfcfbf30056b377663a4
haikaljs <haikaljavascript@gmail.com>
2024-01-08 11:06:50 +0800

    Delete user

---
+++ src/router/users.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+import { deleteUser, getAllUsers } from "../controllers/user";
+import { isAuthenticated } from "../middlewares";
+
+export default (router: express.Router) => {
+  router.get("/users", isAuthenticated, getAllUsers);
+  router.delete("/users/:id", deleteUser);
+};
